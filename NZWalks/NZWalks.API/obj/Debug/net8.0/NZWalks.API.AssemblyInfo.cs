be1124e0e26ff64cd01ace70fe86f5516b17cae3
William Green <will.g.green@gmail.com>
2025-09-19 14:15:25 -0400

    running and calling asp.net core web api

--- NZWalks/NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks/NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263cfa3443d417597900ee7e22936b8e5d68f539")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
