b02cbda3dc201389055597d69d86e511728ea8d5
William Green <will.g.green@gmail.com>
2025-09-22 20:34:33 -0400

    Create New Controller (Regions Controller)

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        // GET ALL REGIONS
+        // GET: https//:localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.pexels.com/photos/33981226/pexels-photo-33981226.jpeg"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.pexels.com/photos/33954807/pexels-photo-33954807.jpeg"
+                }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
