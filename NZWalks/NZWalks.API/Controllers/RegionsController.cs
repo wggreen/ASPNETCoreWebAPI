569d87346a115664786479c836fbfe94d0479f7c
William Green <will.g.green@gmail.com>
2025-09-22 21:00:28 -0400

    Get All Regions Action Method

--- NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
 
 namespace NZWalks.API.Controllers
@@ -9,28 +10,19 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
+        private readonly NZWalksDBContext dBContext;
+
+        public RegionsController(NZWalksDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
         // GET ALL REGIONS
         // GET: https//:localhost:portnumber/api/regions
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = new List<Region>
-            {
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Auckland Region",
-                    Code = "AKL",
-                    RegionImageUrl = "https://images.pexels.com/photos/33981226/pexels-photo-33981226.jpeg"
-                },
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Auckland Region",
-                    Code = "AKL",
-                    RegionImageUrl = "https://images.pexels.com/photos/33954807/pexels-photo-33954807.jpeg"
-                }
-            };
+
 
             return Ok(regions);
         }
